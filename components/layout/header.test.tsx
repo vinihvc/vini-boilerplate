1f2a7464fe0dd13e5e212344b18901d3e452e6c6
Vinicius Vicentini <viniciusvicentini@live.com>
2023-03-13 16:48:41 -0300

    update test

--- components/layout/header.test.tsx
+++ components/layout/header.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, within } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import { describe, expect, test } from "vitest"
 
 import { Header } from "./header"
